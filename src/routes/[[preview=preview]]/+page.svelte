2e36972697bf89ff9894557e3b77f91407476f85
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-15 21:23:29 +0530

    modified home page

--- src/routes/[[preview=preview]]/+page.svelte
+++ src/routes/[[preview=preview]]/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
 	import { SliceZone } from '@prismicio/svelte';
-	import type { PageProps } from './$types';
 
 	import { components } from '$lib/slices';
 
-	const { data }: PageProps = $props();
+	export let data;
 </script>
 
 <SliceZone slices={data.page.data.slices} {components} />
