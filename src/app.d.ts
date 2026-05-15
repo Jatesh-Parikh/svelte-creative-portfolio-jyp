7f7f9d0b2cea55af0ecc0e460f189a47746732c0
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-15 21:16:00 +0530

    add root layout

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,8 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+declare module '@fontsource-variable/urbanist';
+
 declare global {
 	namespace App {
 		// interface Error {}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,35 @@
 <script>
-	import { PrismicPreview } from '@prismicio/svelte/kit';
-	import { page } from '$app/state';
-	import { repositoryName } from '$lib/prismicio';
+	import '../app.css';
+	import '@fontsource-variable/urbanist';
 
-	let { children } = $props();
+	import { PrismicPreview } from '@prismicio/svelte/kit';
+	import { page } from '$app/stores';
+	import { repositoryName } from '$lib/prismicio';
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+
+	export let data;
 </script>
 
 <svelte:head>
-	<title>{page.data.title}</title>
-	{#if page.data.meta_description}
-		<meta name="description" content={page.data.meta_description} />
+	<title>{$page.data.title}</title>
+	{#if $page.data.meta_description}
+		<meta name="description" content={$page.data.meta_description} />
 	{/if}
-	{#if page.data.meta_title}
-		<meta name="og:title" content={page.data.meta_title} />
+	{#if $page.data.meta_title}
+		<meta name="og:title" content={$page.data.meta_title} />
 	{/if}
-	{#if page.data.meta_image}
-		<meta name="og:image" content={page.data.meta_image} />
+	{#if $page.data.meta_image}
+		<meta name="og:image" content={$page.data.meta_image} />
 		<meta name="twitter:card" content="summary_large_image" />
 	{/if}
 </svelte:head>
+
+<Header settings={data.settings} />
 <main>
-	{@render children()}
+	<slot />
 </main>
+<div class="background-gradient absolute inset-0 -z-50 max-h-screen" />
+<div class="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light" />
+<Footer settings={data.settings} />
 <PrismicPreview {repositoryName} />
