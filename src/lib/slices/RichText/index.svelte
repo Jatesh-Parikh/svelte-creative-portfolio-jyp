a0c6baa6d02631382dba2677b7e2e34a054b5f5e
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-15 15:00:58 +0530

    project init

---
+++ src/lib/slices/RichText/index.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { Content } from '@prismicio/client';
+	import { PrismicRichText } from '@prismicio/svelte';
+
+	
+</script>
+
+<section class="container">
+	
+</section>
+
+<style>
+	.container {
+		max-width: 600px;
+		margin: 6em auto;
+		font-family:
+			-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif,
+			'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+	}
+</style>
