ce4eeefa28e02b138a393bf439c3ae543badd291
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-16 10:16:43 +0530

    add RichText slice

--- src/lib/slices/RichText/index.svelte
+++ src/lib/slices/RichText/index.svelte
@@ -2,19 +2,9 @@
 	import type { Content } from '@prismicio/client';
 	import { PrismicRichText } from '@prismicio/svelte';
 
-	
+	export let slice: Content.RichTextSlice;
 </script>
 
-<section class="container">
-	
-</section>
-
-<style>
-	.container {
-		max-width: 600px;
-		margin: 6em auto;
-		font-family:
-			-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif,
-			'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-	}
-</style>
+<div class="max-w-prose">
+	<PrismicRichText field={slice.primary.content} />
+</div>
