6509793e84df155d742f553849287811c3d1d1ce
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-16 16:21:35 +0530

    removed slice-simulator route

--- src/routes/slice-simulator/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import { SliceSimulator, SliceZone } from '@prismicio/svelte';
-	import { components } from '$lib/slices';
-</script>
-
-<!-- Slot syntax is used for backward compatibility with Svelte <=4. -->
-<SliceSimulator let:slices>
-	<SliceZone {slices} {components} />
-</SliceSimulator>
