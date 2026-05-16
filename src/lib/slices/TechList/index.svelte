6823996c1bca1f1cdd680299804e55a3d3fd74d2
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-16 11:15:50 +0530

    add TechList slice

--- src/lib/slices/TechList/index.svelte
+++ src/lib/slices/TechList/index.svelte
@@ -1,15 +1,80 @@
 <script lang="ts">
+	import Bounded from '$lib/components/Bounded.svelte';
+	import Heading from '$lib/components/Heading.svelte';
 	import type { Content } from '@prismicio/client';
+	import { onMount } from 'svelte';
+	import { gsap } from 'gsap';
+	import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+
+	import IconCircle from '~icons/ic/baseline-circle';
+
+	gsap.registerPlugin(ScrollTrigger);
 
 	export let slice: Content.TechListSlice;
+
+	let component: HTMLElement;
+
+	onMount(() => {
+		const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+		if(prefersReducedMotion) {
+			return;
+		}
+
+		const tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: component,
+				start: 'top bottom',
+				end: 'bottom top',
+				scrub: 4,
+			}
+		});
+
+		tl.fromTo(
+			'.tech-row',
+			{
+				x: (index) => {
+					return index % 2 === 0 ? gsap.utils.random(600, 400) : gsap.utils.random(-600, -400);
+				}
+			},
+			{
+				x: (index) => {
+					return index % 2 === 0 ? gsap.utils.random(-600, -400) : gsap.utils.random(600, 400);
+				},
+				ease: 'power1.inOut'
+			}
+		);
+	});
 </script>
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	Placeholder component for {slice.slice_type} (variation: {slice.variation}) slices.
-	<br />
-	<strong>You can edit this slice directly in your code editor.</strong>
-	<!--
-	💡 Use the Prismic MCP server with your code editor
-	📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-	-->
+<section 
+	data-slice-type={slice.slice_type} 
+	data-slice-variation={slice.variation}
+	class="overflow-hidden"
+	bind:this={component}
+>
+	<Bounded as="div">
+		<Heading size="xl" class="mb-8" tag="h2">
+			{slice.primary.heading}
+		</Heading>
+	</Bounded>
+
+	{#each slice.primary.item as { tech_color, tech_name}}
+		<div 
+			class="tech-row mb-8 flex items-center justify-center gap-4 text-slate-700"
+			aria-label={tech_name || undefined}
+		>
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			{#each Array(15) as _, index}
+				<span
+					class="tech_item text-8xl font-extrabold uppercase tracking-tighter"
+					style="color: {index === 7 && tech_color ? tech_color : 'inherit'}"
+				>
+					{tech_name}
+				</span>
+				<span class="text-3xl">
+					<IconCircle />
+				</span>
+			{/each}
+		</div>
+	{/each}
 </section>
