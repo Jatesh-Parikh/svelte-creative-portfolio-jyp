7baaa9b3aa23f1568414d8c3c5fca2a29072d1da
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-16 10:15:02 +0530

    add ImageBlock slice

--- src/lib/slices/ImageBlock/index.svelte
+++ src/lib/slices/ImageBlock/index.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
 	import type { Content } from '@prismicio/client';
+	import { PrismicImage } from '@prismicio/svelte';
 
 	export let slice: Content.ImageBlockSlice;
 </script>
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	Placeholder component for {slice.slice_type} (variation: {slice.variation}) slices.
-	<br />
-	<strong>You can edit this slice directly in your code editor.</strong>
-	<!--
-	💡 Use the Prismic MCP server with your code editor
-	📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-	-->
-</section>
+<PrismicImage 
+	field={slice.primary.image}
+	class="not-prose w-full h-full rounded-md my-10 md:my-14 lg:my-16"
+/>
