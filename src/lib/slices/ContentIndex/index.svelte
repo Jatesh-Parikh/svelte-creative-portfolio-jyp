c89408d315fe5625f3fc34b9c4603120e8129f29
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-16 10:12:40 +0530

    add ContentIndex slice

--- src/lib/slices/ContentIndex/index.svelte
+++ src/lib/slices/ContentIndex/index.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
-	import type { Content } from '@prismicio/client';
+	import Bounded from '$lib/components/Bounded.svelte';
+	import Heading from '$lib/components/Heading.svelte';
+	import { isFilled ,type Content } from '@prismicio/client';
+	import { PrismicRichText } from '@prismicio/svelte';
+	import ContentList from './ContentList.svelte';
 
 	export let slice: Content.ContentIndexSlice;
+
+	export let items: Content.BlogpostDocument[] | Content.ProjectDocument[];
 </script>
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	Placeholder component for {slice.slice_type} (variation: {slice.variation}) slices.
-	<br />
-	<strong>You can edit this slice directly in your code editor.</strong>
-	<!--
-	💡 Use the Prismic MCP server with your code editor
-	📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-	-->
-</section>
+<Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+	<Heading size="xl" class="mb-8">
+		{slice.primary.heading}
+	</Heading>
+	{#if isFilled.richText(slice.primary.description)}
+		<div class="prose prose-xl prose-invert mb-10">
+			<PrismicRichText field={slice.primary.description} />
+		</div>
+	{/if}
+
+	<ContentList 
+		{items}
+		fallbackItemImage={slice.primary.fallback_item_image}
+		viewMoreText={slice.primary.view_more_text}
+	/>
+</Bounded>
